4a8ede0095e182f0b13e8b7ca3b214b0cbb6b90e
Valerie <zoou2foo@gmail.com>
2023-02-04 16:12:55 -0500

    starting philo

---
+++ philo/includes/philosophers.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vjean <vjean@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/04 16:06:00 by vjean             #+#    #+#             */
+/*   Updated: 2023/02/04 16:08:21 by vjean            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <time.h>
+
+/*		section 1		*/
+
+/*		section 2		*/
+
+/*		section 3		*/
+
+#endif
